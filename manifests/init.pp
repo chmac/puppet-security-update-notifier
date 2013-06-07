0009e458b72047ce37fbb4616053335631f2ad03
Callum Macdonald <git@callum-macdonald.com>
2013-06-07 13:40:35 +0200

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+# Class: security_update_notifier
+#
+# This module manages security_update_notifier
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# class { 'security_update_notifier': }
+#
+# [Remember: No empty lines between comments and class definition]
+class security_update_notifier {
+
+	file { '/usr/bin/security-update-notifier.sh':
+		mode			=> 664,
+		source			=> 'puppet:///modules/chamc-security_update_notifier/src/security-update-notifier.sh',
+	}
+
+	cron { 'security-update-notifier':
+		command			=> '/usr/bin/security-update-notifier.sh',
+		hour			=> 4,
+		minute			=> 44,
+	}
+
+}
