d8361661e3d2610bee2c2fc7afcdf2b14b3932a2
Callum Macdonald <git@callum-macdonald.com>
2013-06-29 14:51:10 +0100

    Build v0.0.5.

---
+++ pkg/chmac-security_update_notifier-0.0.5/README.md
@@ -0,0 +1,4 @@
+puppet-security-update-notifier
+===============================
+
+Simple puppet module to install security-update-notifier
---
+++ pkg/chmac-security_update_notifier-0.0.5/manifests/init.pp
@@ -0,0 +1,37 @@
+# Class: security_update_notifier
+#
+# This module manages security_update_notifier
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# class { 'security_update_notifier': }
+#
+# [Remember: No empty lines between comments and class definition]
+class security_update_notifier {
+
+	package { 'sun-update-notifier-common':
+		name			=> 'update-notifier-common',
+	}
+
+	file { '/usr/bin/security-update-notifier.sh':
+		owner			=> 'root',
+		group			=> 'root',
+		mode			=> 755,
+		source			=> 'puppet:///modules/security_update_notifier/src/security-update-notifier.sh',
+		require			=> Package['sun-update-notifier-common'],
+	}
+
+	cron { 'security-update-notifier':
+		command			=> '/usr/bin/security-update-notifier.sh',
+		hour			=> 4,
+		minute			=> 44,
+		require			=> File['/usr/bin/security-update-notifier.sh'],
+	}
+
+}
